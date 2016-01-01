a8307a4d90ab746368d47981de784b2eccdc0c5c
David <david@team-reflex.com>
2016-01-02 11:27:52 +1300

    Improved SetLevel and updated Composer

--- src/Commands/SetLevel.php
+++ src/Commands/SetLevel.php
@@ -18,12 +18,18 @@ class SetLevel
 	public static function handleMessage($message, $params, $discord, $config)
 	{
 		if (isset($params[1])) {
+			$user = $params[1];
 			$level = (isset($params[2])) ? $params[2] : 2;
-			$config['perms']['perms'][$params[1]] = $level;
+
+			if (preg_match('/<@([0-9]+)>/', $user, $matches)) {
+				$user = $matches[1];
+			}
+
+			$config['perms']['perms'][$user] = $level;
 
 			Config::saveConfig($config);
 
-			$message->reply("Set user <@{$params[1]}> auth level to {$level}");
+			$message->reply("Set user <@{$user}> auth level to {$config['perms']['levels'][$level]}");
 		}
 	}
 }
