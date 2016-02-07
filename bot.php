2da25b64bc80bb31c19c87bb6fc22d5aed20e6ba
Comentarinformal <coment@comentarinformal.info>
2016-02-07 06:01:24 +0100

    no message

--- bot.php
+++ bot.php
@@ -32,11 +32,11 @@ try {
 	$bot->addCommand('flush', \Bot\Commands\Flush::class, 2, 'Flushes the channels messages.', '[messages=15]');
 	$bot->addCommand('info', \Bot\Commands\Info::class, 1, 'Shows information about the bot.', '');
 	$bot->addCommand('meme', \Bot\Commands\Meme::class, 1, 'dank memes', '');
-	$bot->addCommand('setlevel', \Bot\Commands\SetLevel::class, 4, 'Sets the auth level of a user.', '<user> [level=2]');
+	$bot->addCommand('setlevel', \Bot\Commands\SetLevel::class, 2, 'Sets the auth level of a user.', '<user> [level=2]');
 	$bot->addCommand('mylevel', \Bot\Commands\MyLevel::class, 0, 'Shows your auth level.', '');
-	$bot->addCommand('setprefix', \Bot\Commands\SetPrefix::class, 4, 'Sets the prefix for the bot.', '<prefix>');
+	$bot->addCommand('setprefix', \Bot\Commands\SetPrefix::class, 2, 'Sets the prefix for the bot.', '<prefix>');
 	$bot->addCommand('userinfo', \Bot\Commands\UserInfo::class, 1, 'Shows information about yourself or the specified user.', '[user]');
-	$bot->addCommand('restart', \Bot\Commands\Restart::class, 4, 'Restarts the bot.', '');
+	$bot->addCommand('restart', \Bot\Commands\Restart::class, 2, 'Restarts the bot.', '');
 	$bot->addCommand('coinflip', \Bot\Commands\Coinflip::class, 1, 'Does a coinflip.', '');
 	$bot->addCommand('8ball', \Bot\Commands\Eightball::class, 1, 'Magic 8 Ball!', '');
 	$bot->addCommand('guilds', \Bot\Commands\Guilds::class, 1, 'Shows all the guilds.', '');
